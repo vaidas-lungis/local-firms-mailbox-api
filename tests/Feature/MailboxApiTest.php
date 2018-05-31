5d563c0a6e91033e6fe3059227bbbc87688be3c6
vaidas-lungis <vaidas.lungis+github@gmail.com>
2018-05-31 10:13:42 +0300

    Create test to cover basic usage

---
+++ tests/Feature/MailboxApiTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Message;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MailboxApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicUsage()
+    {
+        factory(Message::class, 5)->create();
+
+        $result = $this->json('get', route('messages.index'));
+        $this->assertTrue(count($result->json()['data']) === 5);
+
+        $result = $this->json('get', route('archived.index'));
+        $this->assertTrue(count($result->json()['data']) === 0);
+
+        $result  = $this->json('get', route('messages.show', [1]));
+        $message = Message::find(1);
+        $this->assertNotEmpty($result->json()['data']);
+        $this->assertTrue($message->subject == $result->json()['data']['subject']);
+
+        $this->assertNull($result->json()['data']['read_at']);
+        $result = $this->json('post', route('message.read', [1]));
+        $this->assertNotNull($result->json()['data']['read_at']);
+
+        $this->assertNull($result->json()['data']['archived_at']);
+        $result = $this->json('post', route('message.archive', [1]));
+        $this->assertNotNull($result->json()['data']['archived_at']);
+
+        $result = $this->json('get', route('archived.index'));
+        $this->assertTrue(count($result->json()['data']) === 1);
+
+    }
+}
